65ccdb069b66199988dfb1cf84e5bb70b57d9777
Marcvg69 <marc.vangoolen@telenet.be>
2025-06-06 16:08:10 +0200

    Final version for today

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ if __name__ == "__main__":
     filename = sys.argv[1]  # The filename should be passed as a command-line argument
     colleagues = load_colleagues(filename)  # Load the colleague data into a list or similar structure
 
+# Add tables when there are more than 24 people in the list
+if len(colleagues) > 24 and len(colleagues) % 4 == 0:
+    number_of_tables = int(len(colleagues) / 4)
+elif len(colleagues) <= 24:
+    number_of_tables = 6
+else:
+    number_of_tables = int(len(colleagues) / 4) + 1
+    
     # If the colleague list was successfully loaded
     if colleagues:
         # Create an OpenSpace object with a given layout:
         # 6 tables, each with 4 seats (can be adjusted as needed)
-        open_space = OpenSpace(number_of_tables=6, seats_per_table=4)
+        open_space = OpenSpace(number_of_tables, seats_per_table=4)
         
         # Organize the seating: distribute colleagues across tables
         open_space.organize(colleagues)
